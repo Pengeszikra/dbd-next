44ac954a69e8b763a279fa36b65d56cb64d0b8df
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-06-30 16:15:31 +0200

    /api/message dual query

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -6,20 +6,18 @@ export default async function handler(
   res: NextApiResponse
 ) {
   let { db } = await connectToDatabase();
-
-  const listCollection = await db.collection("list")
   
   const {query:{msg, sendBy} = {}} = req;
 
   if (msg && sendBy) {
-    listCollection.insertOne({msg, sendBy});
+    await db.collection("list").insertOne({msg, sendBy});
   }
 
-  const result = await listCollection
+  const result = await db.collection("list")
     .find({})
     .toArray()
   ;
-
+ 
   const resultWithId = result
     .map(({_id, ...rest}) => ({id: _id.toString(), ...rest}))
     .reverse()
