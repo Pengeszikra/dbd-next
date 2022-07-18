752bba836297fc6df39955913d6902b9c8e14bbe
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-07-18 22:42:40 +0200

    manifest link added

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html className="dark">
       <Head>
-        <title>non dbd app</title>
+      <Head>
+        <title>Ex DBD</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="manifest" href="manifest.json"></link>
+      </Head>
       </Head>
       <body className=' dark:bg-slate-900'>
         <Main />
