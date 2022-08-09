438b08eae2413c3c078d9030b1cb5be14d796e21
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-08-09 23:23:38 +0200

    double head removed

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,9 @@ export default function Document() {
   return (
     <Html className="dark">
       <Head>
-      <Head>
         <title>Ex DBD</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="manifest" href="manifest.json"></link>
-      </Head>
       </Head>
       <body className=' dark:bg-slate-900'>
         <Main />
