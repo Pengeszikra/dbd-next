23b5d2349f0ab021d1d0f6e0ac255f76936f76fd
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2022-08-09 23:09:42 +0200

    graphql added

---
+++ graphql/resolvers.ts
@@ -0,0 +1,10 @@
+export const resolvers = {
+  Query: {
+    info: () => [
+      {id: 'some-id-3232', title: '--foo--'},
+      {id: 'some-id-6535', title: 'Another info'},
+      {id: 'some-id-7632', title: 'Maybe this is what I want.'},
+    ]
+    ,
+  }
+};
